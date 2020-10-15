cabbc9b9c69402b30e1002af98c006e9afdd15e6
Joann Delahay <delahay.joann@gmail.com>
2020-10-15 12:35:32 +0200

    3

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,24 @@
     var cannon;
     var obst;
     var ammo;
+    var explo;
+
+    var cannonImage = new Image();
+    cannonImage.src = './img/cannon.png';
+    var obstacleImage = new Image();
+    obstacleImage.src = './img/Discord.png';
+    var ammoImage = new Image();
+    ammoImage.src = './img/ammunition.gif';
+    var background = new Image();
+    background.src = './img/space.jpg';
+    var explosion = new Image();
+    explosion.src = './img/explosion.gif';
+    
 
     async function startGame() {
         await myGameArea.start();
-        cannon = new component(20, 80, "red", 310, 380);
-        obst = new obstacle(50,10,"blue");
+        cannon = new component(52, 55, 298, 425);
+        obst = new obstacle(50,50);
       }
       
       var myGameArea = {
@@ -15,25 +28,29 @@
           this.canvas.width = 640;
           this.canvas.height = 480;
           this.context = this.canvas.getContext("2d");
+          this.canvas.fillStyle = "blue";
+          this.context.drawImage(background,0,0);
           document.body.insertBefore(this.canvas, document.body.childNodes[0]);
           this.interval = setInterval(updateGameArea, 10);
 
           window.addEventListener('keydown', function (e) {
-            myGameArea.key = e.key;
+            myGameArea.keys = (myGameArea.keys || []);
+            myGameArea.keys[e.key] = true;
           })
           window.addEventListener('keyup', function (e) {
-            myGameArea.key = false;
-          }) 
+            myGameArea.keys[e.key] = false;
+          })
         },
         clear : function() {
             this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.context.drawImage(background,0,0);
         },
         stop : function() {
             clearInterval(this.interval);
         }
       }
 
-    function component(width, height, color, x, y) {
+    function component(width, height, x, y) {
         this.width = width;
         this.height = height;
         this.speedX = 0;
@@ -42,19 +59,21 @@
         this.y = y;
         this.update = function(){
             ctx = myGameArea.context;
-            ctx.fillStyle = color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
+            ctx.drawImage(cannonImage,
+              this.x,
+              this.y,
+              this.width, this.height);
           }
         this.newPos = function() {
             this.x += this.speedX;
             this.y += this.speedY;
         }
         this.shoot = function() {
-            ammo = new projectile(5,10,"black", this.x, this.y, this.width);
+            ammo = new projectile(30,60, this.x, this.y, this.width);
         }
     }
 
-    function obstacle(width, height, color){
+    function obstacle(width, height){
         this.width = width;
         this.height = height;
         this.x = Math.trunc(Math.random()* (myGameArea.canvas.width - this.width));
@@ -62,12 +81,23 @@
 
         this.update = function(){
             ctx = myGameArea.context;
-            ctx.fillStyle = color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
+            ctx.drawImage(obstacleImage,
+              this.x,
+              this.y,
+              this.width, this.height);
           }
     }
 
-    function projectile(width, height, color, x, y, cannonW){
+    function explose(obst){
+      this.count = 0;
+      this.update = function(){
+        ctx = myGameArea.context;
+        ctx.drawImage(explosion,obst.x-65,obst.y-65,200,200);
+        this.count += 1;
+      }
+    }
+
+    function projectile(width, height, x, y, cannonW){
         this.width = width;
         this.height = height;
         this.speedY = -5;
@@ -76,21 +106,48 @@
 
         this.update = function(){
             ctx = myGameArea.context;
-            ctx.fillStyle = color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
+            ctx.drawImage(ammoImage,
+              this.x,
+              this.y,
+              this.width, this.height);
           }
         this.newPos = function() {
             this.y += this.speedY;
-        } 
+        }
+        this.crashWith = function(otherobj) {
+          var myleft = this.x;
+          var myright = this.x + (this.width);
+          var mytop = this.y;
+          var mybottom = this.y + (this.height);
+          var otherleft = otherobj.x;
+          var otherright = otherobj.x + (otherobj.width);
+          var othertop = otherobj.y;
+          var otherbottom = otherobj.y + (otherobj.height);
+          var crash = true;
+          if ((mybottom < othertop) ||
+          (mytop > otherbottom) ||
+          (myright < otherleft) ||
+          (myleft > otherright)) {
+            crash = false;
+          }
+          return crash;
+        }
     }
 
     function updateGameArea() {
+      if (explo && explo.count >= 30){explo = null;}
+      if (ammo && ammo.y < 0){ammo = null;}
+      if (ammo && ammo.crashWith(obst)) {
+        explo = new explose(obst);
+        obst = new obstacle(50,50);
+        ammo = null;
+      } else {
         myGameArea.clear();
         cannon.speedX = 0;
         cannon.speedY = 0;
-        if (myGameArea.key && (myGameArea.key == 'ArrowLeft')) {cannon.speedX = -3; }
-        if (myGameArea.key && (myGameArea.key == 'ArrowRight')) {cannon.speedX = 3; }
-        if (myGameArea.key && (myGameArea.key == ' ')) {cannon.shoot(); }
+        if (myGameArea.keys && (myGameArea.keys['ArrowLeft']) && cannon.x >= 0) {cannon.speedX = -5; }
+        if (myGameArea.keys && (myGameArea.keys['ArrowRight']) && cannon.x <= (myGameArea.canvas.width-cannon.width)) {cannon.speedX = 5; }
+        if (myGameArea.keys && (myGameArea.keys[' '])) {if(!ammo)cannon.shoot(); }
         cannon.newPos();
         cannon.update();
         obst.update();
@@ -98,6 +155,10 @@
             ammo.newPos();
             ammo.update();
         }
+        if (explo){
+          explo.update();
+        }
+      }
     }
       
     
